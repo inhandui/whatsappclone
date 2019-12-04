71d8a34fcd0339934b984ea17ae4a6b2e7a8aa1d
Inhanduí <inhandui@gmail.com>
2019-12-04 15:30:14 -0300

    Add firebase component, change layout to Material Design and add logo asset.

--- app/src/main/java/com/cursoandroid/whatsappclone/MainActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseReference.child("Pontos").setValue(100);
     }
 }
