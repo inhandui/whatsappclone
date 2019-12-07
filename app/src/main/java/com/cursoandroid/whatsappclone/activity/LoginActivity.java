bf9745d4679f6ea36d67fde46922f5bb6c217c11
Inhanduí <inhandui@gmail.com>
2019-12-07 18:15:18 -0300

    Adding login screen.

---
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cursoandroid.whatsappclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cursoandroid.whatsappclone.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
