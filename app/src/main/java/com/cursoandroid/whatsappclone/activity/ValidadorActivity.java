9ee38a9f7790e252f71bd19315b788bfa4fe7764
Inhanduí <inhandui@gmail.com>
2019-12-06 21:19:55 -0300

    Implementing token validation.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/ValidadorActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/ValidadorActivity.java
@@ -6,13 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cursoandroid.whatsappclone.R;
+import com.cursoandroid.whatsappclone.helper.Preferencias;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.HashMap;
 
 public class ValidadorActivity extends AppCompatActivity {
 
     private EditText txtCodigo;
     private Button btnValidar;
+
+    private SimpleMaskFormatter simpleMaskFormatter;
+    private MaskTextWatcher maskTWValidacao;
+
+    private Preferencias preferencias;
+    HashMap<String, String> dadosUsuario;
+
+    private String tokenGerado, tokenUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +36,25 @@ public class ValidadorActivity extends AppCompatActivity {
         txtCodigo = findViewById(R.id.txtCodigo);
         btnValidar = findViewById(R.id.btnValidar);
 
+        simpleMaskFormatter = new SimpleMaskFormatter("NNNN");
+        maskTWValidacao = new MaskTextWatcher(txtCodigo, simpleMaskFormatter);
+        txtCodigo.addTextChangedListener(maskTWValidacao);
+
         btnValidar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                preferencias = new Preferencias(ValidadorActivity.this);
+                dadosUsuario = preferencias.getDadosUsuario();
 
+                tokenGerado = dadosUsuario.get("token");
+                tokenUsuario = txtCodigo.getText().toString();
+
+                if (tokenUsuario.equals(tokenGerado)){
+                    Toast.makeText(ValidadorActivity.this, R.string.token_ok, Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(ValidadorActivity.this, R.string.erro_token, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
