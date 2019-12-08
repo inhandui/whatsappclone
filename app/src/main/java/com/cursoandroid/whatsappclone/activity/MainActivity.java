e3f969534d7c4b8a693be0b2186c8b3140307797
Inhanduí <inhandui@gmail.com>
2019-12-08 01:57:39 -0300

    Implementing firebase config.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cursoandroid.whatsappclone.R;
+import com.cursoandroid.whatsappclone.data.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        databaseReference = ConfiguracaoFirebase.getFirebase();
+//        databaseReference.child("pontos").setValue("800");
     }
 }
---
+++ app/src/main/java/com/cursoandroid/whatsappclone/data/ConfiguracaoFirebase.java
@@ -0,0 +1,16 @@
+package com.cursoandroid.whatsappclone.data;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public final class ConfiguracaoFirebase {
+    private static  DatabaseReference databaseReference;
+
+    public static DatabaseReference getFirebase(){
+        if (databaseReference == null){
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return databaseReference;
+    }
+}
