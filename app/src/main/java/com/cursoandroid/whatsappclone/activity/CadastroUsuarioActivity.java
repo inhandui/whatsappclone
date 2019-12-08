051bb346b1595a9abdbda68f86f514b13cf09a6d
Inhanduí <inhandui@gmail.com>
2019-12-08 02:03:34 -0300

    Adding screen elements reference.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/CadastroUsuarioActivity.java
@@ -3,14 +3,34 @@ package com.cursoandroid.whatsappclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cursoandroid.whatsappclone.R;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
+    private EditText txtNome;
+    private EditText txtEmail;
+    private EditText txtSenha;
+    private Button btnCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        txtNome = findViewById(R.id.txtNome);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
