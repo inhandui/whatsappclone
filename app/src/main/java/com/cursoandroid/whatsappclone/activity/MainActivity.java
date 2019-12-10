5525388934d9c81dfd90beddda03ebe2a6544591
Inhanduí <inhandui@gmail.com>
2019-12-10 03:12:43 -0300

    Change design attributes and implementing user logout menu action.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package com.cursoandroid.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.cursoandroid.whatsappclone.R;
+import com.cursoandroid.whatsappclone.data.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
 
+        firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
     }
 
     @Override
@@ -30,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sair:
+                deslogarUsuario();
+                return true;
+            case R.id.pesquisar:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    public void deslogarUsuario(){
+        firebaseAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
