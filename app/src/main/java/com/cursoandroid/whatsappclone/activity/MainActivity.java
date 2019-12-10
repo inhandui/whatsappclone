d3b52f4ad62ecbbd12990c52129dd218f5a388d9
Inhanduí <inhandui@gmail.com>
2019-12-10 02:50:40 -0300

    Adding activity main xml design elements.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/MainActivity.java
@@ -1,38 +1,34 @@
 package com.cursoandroid.whatsappclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.cursoandroid.whatsappclone.R;
-import com.cursoandroid.whatsappclone.data.ConfiguracaoFirebase;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSair;
-    private FirebaseAuth firebaseAuth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSair = findViewById(R.id.btnSair);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
 
-        btnSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
-                firebaseAuth.signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
