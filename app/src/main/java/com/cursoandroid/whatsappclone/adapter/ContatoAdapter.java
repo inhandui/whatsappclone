b9e8f08b995f3172d444a1b661b8e98c3eace818
Inhanduí <inhandui@gmail.com>
2020-06-28 13:33:27 -0300

    Adding class attributes.

--- app/src/main/java/com/cursoandroid/whatsappclone/adapter/ContatoAdapter.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/adapter/ContatoAdapter.java
@@ -12,7 +12,14 @@ import java.util.ArrayList;
 
 public class ContatoAdapter extends ArrayAdapter<Contato> {
 
+    private Context context;
+    private ArrayList<Contato> contatos;
+
     public ContatoAdapter(@NonNull Context context, @NonNull ArrayList<Contato> objects) {
         super(context, 0, objects);
+
+        this.context = context;
+
+        this.contatos = objects;
     }
 }
