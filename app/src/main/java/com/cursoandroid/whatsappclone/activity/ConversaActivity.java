dc65ac75eb99159584466a3c98b40675da0339f3
Inhanduí <inhandui@gmail.com>
2020-07-10 23:56:18 -0300

    Adding basic design to Conversa Activity.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cursoandroid.whatsappclone.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class ConversaActivity extends AppCompatActivity {
+
+    private MaterialToolbar appToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversa);
+
+        /* Adding layout elements reference */
+        appToolbar = findViewById(R.id.tb_conversa);
+
+        //set toolbar
+        appToolbar.setTitle("Fulano");
+        setSupportActionBar(appToolbar);
+
     }
 }
