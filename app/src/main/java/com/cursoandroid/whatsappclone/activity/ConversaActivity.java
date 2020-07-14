f2b8c726de3f1cde302ee779d37195344ffda393
Inhanduí <inhandui@gmail.com>
2020-07-14 00:21:25 -0300

    Basic get message data process from firebase.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
@@ -1,11 +1,14 @@
 package com.cursoandroid.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.cursoandroid.whatsappclone.R;
@@ -14,7 +17,13 @@ import com.cursoandroid.whatsappclone.data.ConfiguracaoFirebase;
 import com.cursoandroid.whatsappclone.data.Mensagem;
 import com.cursoandroid.whatsappclone.data.Preferencias;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.EventListener;
 
 public class ConversaActivity extends AppCompatActivity {
 
@@ -23,9 +32,13 @@ public class ConversaActivity extends AppCompatActivity {
     private EditText txt_mensagem;
     private ImageButton btn_enviar;
     private String texto_mensagem;
+    private ListView lv_mensagens;
 
-    //Data
+    //Activity Data
     private Mensagem mensagem;
+    private ArrayList<String> mensagens;
+    private ArrayAdapter arrayAdapter;
+    private ValueEventListener valueEventListenerMensagem;
 
     //User data
     private Preferencias preferencias;
@@ -49,6 +62,8 @@ public class ConversaActivity extends AppCompatActivity {
         appToolbar = findViewById(R.id.tb_conversa);
         txt_mensagem = findViewById(R.id.mensagem);
         btn_enviar = findViewById(R.id.btn_enviar);
+        lv_mensagens = findViewById(R.id.lv_mensagens);
+
 
         /* get user data */
         preferencias = new Preferencias(ConversaActivity.this);
@@ -67,11 +82,43 @@ public class ConversaActivity extends AppCompatActivity {
         else {
             appToolbar.setTitle("Undefined");
         }
-
-
         setSupportActionBar(appToolbar);
 
-        //setup event for the button.
+        /* Set listview messages */
+        mensagens = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter(ConversaActivity.this, android.R.layout.simple_list_item_1);
+        lv_mensagens.setAdapter( arrayAdapter );
+
+        //Retrieve messagens from firebase
+        databaseReference = ConfiguracaoFirebase.getFirebase().child("mensagens").child(idRemetente).child(idDestinatario);
+
+        //Setup messages event listener
+        valueEventListenerMensagem = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //clear messages
+                mensagens.clear();
+
+                //retrieve messages
+                for (DataSnapshot dados: dataSnapshot.getChildren()){
+                    mensagem = dados.getValue( Mensagem.class );
+                    mensagens.add(mensagem.getMensagem());
+                }
+
+                //notify
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        //Add value event listener to firebase reference
+        databaseReference.addValueEventListener( valueEventListenerMensagem );
+
+        //setup event for the send button.
         btn_enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,6 +137,9 @@ public class ConversaActivity extends AppCompatActivity {
 
                     //Save data message
                     salvarMensagem(idDestinatario, mensagem);
+
+                    //delete data from message EditText
+                    txt_mensagem.setText("");
                 }
             }
         });
@@ -109,5 +159,12 @@ public class ConversaActivity extends AppCompatActivity {
 
 
         return retorno;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        databaseReference.removeEventListener( valueEventListenerMensagem );
     }
 }
