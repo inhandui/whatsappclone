17a1be7a39ff8ea62b577a0da5d7f5fd711e8ace
Inhanduí <inhandui@gmail.com>
2020-07-13 23:53:00 -0300

    Save message to firebase.

--- app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
+++ app/src/main/java/com/cursoandroid/whatsappclone/activity/ConversaActivity.java
@@ -3,13 +3,42 @@ package com.cursoandroid.whatsappclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.cursoandroid.whatsappclone.R;
+import com.cursoandroid.whatsappclone.data.Base64Custom;
+import com.cursoandroid.whatsappclone.data.ConfiguracaoFirebase;
+import com.cursoandroid.whatsappclone.data.Mensagem;
+import com.cursoandroid.whatsappclone.data.Preferencias;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.database.DatabaseReference;
 
 public class ConversaActivity extends AppCompatActivity {
 
+    /* Interface UI*/
     private MaterialToolbar appToolbar;
+    private EditText txt_mensagem;
+    private ImageButton btn_enviar;
+    private String texto_mensagem;
+
+    //Data
+    private Mensagem mensagem;
+
+    //User data
+    private Preferencias preferencias;
+    private String idRemetente;
+
+    //Receiver user data
+    private String nomeDestinatario;
+    private String idDestinatario;
+    private String emailDestinatario;
+
+    //Firebase reference
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +47,22 @@ public class ConversaActivity extends AppCompatActivity {
 
         /* Adding layout elements reference */
         appToolbar = findViewById(R.id.tb_conversa);
+        txt_mensagem = findViewById(R.id.mensagem);
+        btn_enviar = findViewById(R.id.btn_enviar);
 
+        /* get user data */
+        preferencias = new Preferencias(ConversaActivity.this);
+        idRemetente = preferencias.getidUsuario();
+
+        /* Get contato data and set toolbar title*/
         Bundle extra = getIntent().getExtras();
 
         if (extra != null){
             //set toolbar title
-            appToolbar.setTitle(extra.getString("nome"));
+            nomeDestinatario = extra.getString("nome");
+            emailDestinatario = extra.getString("email");
+            idDestinatario = Base64Custom.codificarBase64(emailDestinatario);
+            appToolbar.setTitle(nomeDestinatario);
         }
         else {
             appToolbar.setTitle("Undefined");
@@ -32,5 +71,43 @@ public class ConversaActivity extends AppCompatActivity {
 
         setSupportActionBar(appToolbar);
 
+        //setup event for the button.
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get message
+                texto_mensagem = txt_mensagem.getText().toString();
+
+                if (texto_mensagem.isEmpty() ){
+                    Toast.makeText(ConversaActivity.this, R.string.erro_mensagem_vazia, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    /* Setting up data message */
+                    mensagem = new Mensagem();
+                    mensagem.setId(idRemetente);
+                    mensagem.setMensagem(texto_mensagem);
+
+                    //Save data message
+                    salvarMensagem(idDestinatario, mensagem);
+                }
+            }
+        });
+
+    }
+
+    private boolean salvarMensagem(String idDestinatario, Mensagem mensagem){
+        boolean retorno = false;
+        try {
+            databaseReference = ConfiguracaoFirebase.getFirebase().child("mensagens");
+            databaseReference.child(mensagem.getId()).child(idDestinatario).push().setValue(mensagem);
+            retorno = true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return retorno;
     }
 }
---
+++ app/src/main/java/com/cursoandroid/whatsappclone/data/Mensagem.java
@@ -0,0 +1,25 @@
+package com.cursoandroid.whatsappclone.data;
+
+public class Mensagem {
+    private String id;
+    private String mensagem;
+
+    public Mensagem() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
